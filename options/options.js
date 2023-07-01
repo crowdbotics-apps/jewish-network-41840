301de22b8faba51eb29d3c57388cef75104a3d1d
Crowdbotics <team@crowdbotics.com>
2023-07-01 07:09:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jewish-network-41840",
+  url: "https://jewish-network-41840.botics.co",
+  api: "https://jewish-network-41840.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
